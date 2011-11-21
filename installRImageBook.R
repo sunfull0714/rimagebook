d42fedc217d4456edd56a6c63b4405d2d77692a2
takeo.katsuki@gmail.com <takeo.katsuki@gmail.com@3080b295-6217-cb68-dcd9-4f8b48e4c1b6>
2011-11-21 16:18:16 +0000

    Escape SSL certification problem.

--- installRImageBook.R
+++ installRImageBook.R
@@ -6,10 +6,10 @@ installRImageBook <- function(){
   OS <- Sys.getenv("OS")
   if(regexpr("win", Sys.getenv("OS"), ignore.case=TRUE) > 0){
     file <- "RImageBook_0.1.zip"
-    url <- sprintf("https://rimagebook.googlecode.com/files/%s",file)
+    url <- sprintf("http://rimagebook.googlecode.com/files/%s",file)
   } else {
-    file <- "biOps_0.2.1.1.tar.gz"
-    url <- sprintf("http://cran.r-project.org/src/contrib/%s",file)
+    file <- "RImageBook_0.1.tar.gz"
+    url <- sprintf("http://rimagebook.googlecode.com/files/%s",file)
   }
   content <- getBinaryURL(url)
   tempfile <- sprintf("%s\\%s",tempdir(),file)
