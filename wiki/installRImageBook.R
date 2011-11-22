f4654ca61402332fcfe8f632e3292bcf486bbc62
takeo.katsuki@gmail.com <takeo.katsuki@gmail.com@3080b295-6217-cb68-dcd9-4f8b48e4c1b6>
2011-11-22 05:39:37 +0000

    Added installation script. For test.

---
+++ wiki/installRImageBook.R
@@ -0,0 +1,18 @@
+installRImageBook <- function(){
+  if(!require(RCurl)){
+    install.packages("RCurl")
+    library("RCurl")
+  }
+  OS <- Sys.getenv("OS")
+  if(regexpr("win", Sys.getenv("OS"), ignore.case=TRUE) > 0){
+    file <- "RImageBook_0.1.zip"
+    url <- sprintf("http://rimagebook.googlecode.com/files/%s",file)
+  } else {
+    file <- "RImageBook_0.1.tar.gz"
+    url <- sprintf("http://rimagebook.googlecode.com/files/%s%s",file)
+  }
+  content <- getBinaryURL(url)
+  tempfile <- sprintf("%s\\%s",tempdir(),file)
+  writeBin(content, tempfile)
+  install.packages(tempfile, repos=NULL)
+}
