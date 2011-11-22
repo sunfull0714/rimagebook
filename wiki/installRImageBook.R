ac915e97f8931d4f71fa546f04c0133189b1657d
takeo.katsuki@gmail.com <takeo.katsuki@gmail.com@3080b295-6217-cb68-dcd9-4f8b48e4c1b6>
2011-11-22 15:45:15 +0000

    Added package type argument.

--- wiki/installRImageBook.R
+++ wiki/installRImageBook.R
@@ -7,12 +7,14 @@ installRImageBook <- function(){
   if(regexpr("win", Sys.getenv("OS"), ignore.case=TRUE) > 0){
     file <- "RImageBook_0.1.zip"
     url <- sprintf("http://rimagebook.googlecode.com/files/%s",file)
+    pkgtype <- "win.binary"
   } else {
     file <- "RImageBook_0.2.tar.gz"
     url <- sprintf("http://rimagebook.googlecode.com/files/%s",file)
+    pkgtype <- "source"
   }
   content <- getBinaryURL(url)
   tempfile <- sprintf("%s\\%s",tempdir(),file)
   writeBin(content, tempfile)
-  install.packages(tempfile, repos=NULL)
+  install.packages(tempfile, repos=NULL, type=pkgtype)
 }
